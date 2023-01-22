09c5dfefcd4939680fe2a75219c53165a993f4a4
fatimaAfzaal <fatimaafzaal2002@gmail.com>
2023-01-22 13:03:50 +0500

    Add project files.

---
+++ BookShopApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShopApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "These are the details about us You can know more about us by clicking on links below";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "This is where you can contact us and discuss whatever you want";
+
+            return View();
+        }
+    }
+}
